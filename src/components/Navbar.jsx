d4849336829acf21a92306ea863ddc0fc3e33354
Juan C. Toloy <toloyjc@gmail.com>
2023-07-11 17:47:53 -0300

    improvement: creating navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="absolute top-0 w-full p-4 flex items-center justify-between">
+      <img src="/img/logo.png" alt="Backstage Talks logo" className="w-64" />
+      <a href="#" className="hidden lg:block font-bold text-lg hover:underline">
+        info@backstagetalks.com
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
